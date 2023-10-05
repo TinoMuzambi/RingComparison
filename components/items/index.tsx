c6dafc3aea3f56547bcb73f24cafa678b502a12c
Tino <tino.muzambi@uct.ac.za>
2023-10-05 16:34:10 +0200

    Makes table scrollable.

--- components/items/index.tsx
+++ components/items/index.tsx
@@ -4,93 +4,99 @@ import { ItemsInterface } from "@/interfaces";
 
 const Items: React.FC<ItemsInterface> = ({ items }) => {
 	return (
-		<table>
-			<thead className="font-bold border-b pb-2">
-				<tr>
-					<th>Retailer</th>
-					<th>Diamond Carat Weight</th>
-					<th>Diamond Type</th>
-					<th>Diamond Colour</th>
-					<th>Diamond Clarity</th>
-					<th>Diamond</th>
-					<th>Metal</th>
-					<th>Engraving Included</th>
-					<th>Price</th>
-					<th>Warranty</th>
-					<th>Certificate Number</th>
-					<th>Box</th>
-					<th>Payment Options</th>
-					<th>Payment Terms</th>
-					<th>Manufacturing Timeframe</th>
-					<th>Delivery Timeframe</th>
-					<th>Link</th>
-					<th>Reviews</th>
-				</tr>
-			</thead>
-			<tbody>
-				{items.map((item, key) => (
-					<tr className="border-b py-2 last:border-none" key={key}>
-						<td>{item.retailer}</td>
-						<td>{item.diamond.carat_weight}ct</td>
-						<td>{item.diamond.type}</td>
-						<td>{item.diamond.colour}</td>
-						<td>{item.diamond.clarity}</td>
-						<td>
-							{item.diamond.file?.type === "photo" ? (
-								<Image
-									src={`/media/${item.diamond.file?.name}`}
-									width={200}
-									height={200}
-									alt={item.retailer}
-								/>
-							) : item.diamond.file?.type === "video" ? (
-								<video src={`/media/${item.diamond.file?.name}`} controls />
-							) : null}
-						</td>
-						<td>{item.metal}</td>
-						<td>{item.engraving ? "Yes" : "No"}</td>
-						<td>
-							{Intl.NumberFormat("en-US", {
-								style: "currency",
-								currency: "ZAR",
-								maximumFractionDigits: 0,
-								currencyDisplay: "narrowSymbol",
-							}).format(item.price)}
-						</td>
-						<td>{item.warranty}</td>
-						<td>{item?.certificate}</td>
-						<td>
-							{item.box?.type === "photo" ? (
-								<Image
-									src={`/media/${item.box?.file}`}
-									width={200}
-									height={200}
-									// className="w-40 h-40 object-contain"
-									alt={item.retailer}
-								/>
-							) : item.box?.type === "video" ? (
-								<video src={`/media/${item.box?.file}`} controls />
-							) : null}
-						</td>
-						<td>{item.payment.options.join(", ")}</td>
-						<td>{item.payment.terms}</td>
-						<td>{item.manufacturing_timeframe}</td>
-						<td>{item.delivery_timeframe}</td>
-						<td>
-							<a href={item.link} target="_blank">
-								{item.link}
-							</a>
-						</td>
-						<td>
-							<a
-								href={item.reviews.link}
-								target="_blank"
-							>{`${item.reviews.num_reviews} ${item.reviews.type} reviews: ${item.reviews.rating}`}</a>
-						</td>
+		<div className="w-screen overflow-x-auto pr-16 py-2">
+			<table className="w-[300vw] border-collapse border-spacing-8">
+				<thead className="font-bold border-b pb-2">
+					<tr>
+						<th className="p-2">Retailer</th>
+						<th className="p-2">Diamond Carat Weight</th>
+						<th className="p-2">Diamond Type</th>
+						<th className="p-2">Diamond Colour</th>
+						<th className="p-2">Diamond Clarity</th>
+						<th className="p-2">Diamond</th>
+						<th className="p-2">Metal</th>
+						<th className="p-2">Engraving Included</th>
+						<th className="p-2">Price</th>
+						<th className="p-2">Warranty</th>
+						<th className="p-2">Certificate Number</th>
+						<th className="p-2">Box</th>
+						<th className="p-2">Payment Options</th>
+						<th className="p-2">Payment Terms</th>
+						<th className="p-2">Manufacturing Timeframe</th>
+						<th className="p-2">Delivery Timeframe</th>
+						<th className="p-2">Link</th>
+						<th className="p-2">Reviews</th>
 					</tr>
-				))}
-			</tbody>
-		</table>
+				</thead>
+				<tbody>
+					{items.map((item, key) => (
+						<tr className="border-b py-2 last:border-none" key={key}>
+							<td className="p-2">{item.retailer}</td>
+							<td className="p-2">{item.diamond.carat_weight}ct</td>
+							<td className="p-2">{item.diamond.type}</td>
+							<td className="p-2">{item.diamond.colour}</td>
+							<td className="p-2">{item.diamond.clarity}</td>
+							<td className="p-2">
+								{item.diamond.file?.type === "photo" ? (
+									<Image
+										src={`/media/${item.diamond.file?.name}`}
+										width={200}
+										height={200}
+										alt={item.retailer}
+									/>
+								) : item.diamond.file?.type === "video" ? (
+									<video
+										width={200}
+										src={`/media/${item.diamond.file?.name}`}
+										controls
+									/>
+								) : null}
+							</td>
+							<td className="p-2">{item.metal}</td>
+							<td className="p-2">{item.engraving ? "Yes" : "No"}</td>
+							<td className="p-2">
+								{Intl.NumberFormat("en-US", {
+									style: "currency",
+									currency: "ZAR",
+									maximumFractionDigits: 0,
+									currencyDisplay: "narrowSymbol",
+								}).format(item.price)}
+							</td>
+							<td className="p-2">{item.warranty}</td>
+							<td className="p-2">{item?.certificate}</td>
+							<td className="p-2">
+								{item.box?.type === "photo" ? (
+									<Image
+										src={`/media/${item.box?.file}`}
+										width={200}
+										height={200}
+										className=""
+										alt={item.retailer}
+									/>
+								) : item.box?.type === "video" ? (
+									<video src={`/media/${item.box?.file}`} controls />
+								) : null}
+							</td>
+							<td className="p-2">{item.payment.options.join(", ")}</td>
+							<td className="p-2">{item.payment.terms}</td>
+							<td className="p-2">{item.manufacturing_timeframe}</td>
+							<td className="p-2">{item.delivery_timeframe}</td>
+							<td className="p-2">
+								<a href={item.link} target="_blank">
+									{item.link}
+								</a>
+							</td>
+							<td className="p-2">
+								<a
+									href={item.reviews.link}
+									target="_blank"
+								>{`${item.reviews.num_reviews} ${item.reviews.type} reviews: ${item.reviews.rating}`}</a>
+							</td>
+						</tr>
+					))}
+				</tbody>
+			</table>
+		</div>
 	);
 };
 
