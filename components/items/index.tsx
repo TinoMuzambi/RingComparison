197576936c1e4739a65e4acc2fb847e32a96e00f
Tino <tino.muzambi@uct.ac.za>
2023-10-05 16:25:37 +0200

    Converts grid to table.

--- components/items/index.tsx
+++ components/items/index.tsx
@@ -4,92 +4,93 @@ import { ItemsInterface } from "@/interfaces";
 
 const Items: React.FC<ItemsInterface> = ({ items }) => {
 	return (
-		<>
-			<div
-				className="grid font-bold border-b pb-2"
-				style={{ gridTemplateColumns: "repeat(18, minmax(0, 1fr))" }}
-			>
-				<p>Retailer</p>
-				<p>Diamond Carat Weight</p>
-				<p>Diamond Type</p>
-				<p>Diamond Colour</p>
-				<p>Diamond Clarity</p>
-				<p>Diamond</p>
-				<p>Metal</p>
-				<p>Engraving Included</p>
-				<p>Price</p>
-				<p>Warranty</p>
-				<p>Certificate Number</p>
-				<p>Box</p>
-				<p>Payment Options</p>
-				<p>Payment Terms</p>
-				<p>Manufacturing Timeframe</p>
-				<p>Delivery Timeframe</p>
-				<p>Link</p>
-				<p>Reviews</p>
-			</div>
-			{items.map((item, key) => (
-				<div
-					className="grid gap-1 border-b py-2 last:border-none"
-					style={{ gridTemplateColumns: "repeat(18, minmax(0, 1fr))" }}
-					key={key}
-				>
-					<p>{item.retailer}</p>
-					<p>{item.diamond.carat_weight}ct</p>
-					<p>{item.diamond.type}</p>
-					<p>{item.diamond.colour}</p>
-					<p>{item.diamond.clarity}</p>
-					<div>
-						{item.diamond.file?.type === "photo" ? (
-							<Image
-								src={`/media/${item.diamond.file?.name}`}
-								width={200}
-								height={200}
-								alt={item.retailer}
-							/>
-						) : item.diamond.file?.type === "video" ? (
-							<video src={`/media/${item.diamond.file?.name}`} controls />
-						) : null}
-					</div>
-					<p>{item.metal}</p>
-					<p>{item.engraving ? "Yes" : "No"}</p>
-					<p>
-						{Intl.NumberFormat("en-US", {
-							style: "currency",
-							currency: "ZAR",
-							maximumFractionDigits: 0,
-							currencyDisplay: "narrowSymbol",
-						}).format(item.price)}
-					</p>
-					<p>{item.warranty}</p>
-					<p>{item?.certificate}</p>
-					<div>
-						{item.box?.type === "photo" ? (
-							<Image
-								src={`/media/${item.box?.file}`}
-								width={200}
-								height={200}
-								alt={item.retailer}
-							/>
-						) : item.box?.type === "video" ? (
-							<video src={`/media/${item.box?.file}`} controls />
-						) : null}
-					</div>
-					<p>{item.payment.options.join(", ")}</p>
-					<p>{item.payment.terms}</p>
-					<p>{item.manufacturing_timeframe}</p>
-					<p>{item.delivery_timeframe}</p>
-					<a href={item.link} target="_blank" className="break-words">
-						{item.link}
-					</a>
-					<a
-						href={item.reviews.link}
-						target="_blank"
-						className="break-words"
-					>{`${item.reviews.num_reviews} ${item.reviews.type} reviews: ${item.reviews.rating}`}</a>
-				</div>
-			))}
-		</>
+		<table>
+			<thead className="font-bold border-b pb-2">
+				<tr>
+					<th>Retailer</th>
+					<th>Diamond Carat Weight</th>
+					<th>Diamond Type</th>
+					<th>Diamond Colour</th>
+					<th>Diamond Clarity</th>
+					<th>Diamond</th>
+					<th>Metal</th>
+					<th>Engraving Included</th>
+					<th>Price</th>
+					<th>Warranty</th>
+					<th>Certificate Number</th>
+					<th>Box</th>
+					<th>Payment Options</th>
+					<th>Payment Terms</th>
+					<th>Manufacturing Timeframe</th>
+					<th>Delivery Timeframe</th>
+					<th>Link</th>
+					<th>Reviews</th>
+				</tr>
+			</thead>
+			<tbody>
+				{items.map((item, key) => (
+					<tr className="border-b py-2 last:border-none" key={key}>
+						<td>{item.retailer}</td>
+						<td>{item.diamond.carat_weight}ct</td>
+						<td>{item.diamond.type}</td>
+						<td>{item.diamond.colour}</td>
+						<td>{item.diamond.clarity}</td>
+						<td>
+							{item.diamond.file?.type === "photo" ? (
+								<Image
+									src={`/media/${item.diamond.file?.name}`}
+									width={200}
+									height={200}
+									alt={item.retailer}
+								/>
+							) : item.diamond.file?.type === "video" ? (
+								<video src={`/media/${item.diamond.file?.name}`} controls />
+							) : null}
+						</td>
+						<td>{item.metal}</td>
+						<td>{item.engraving ? "Yes" : "No"}</td>
+						<td>
+							{Intl.NumberFormat("en-US", {
+								style: "currency",
+								currency: "ZAR",
+								maximumFractionDigits: 0,
+								currencyDisplay: "narrowSymbol",
+							}).format(item.price)}
+						</td>
+						<td>{item.warranty}</td>
+						<td>{item?.certificate}</td>
+						<td>
+							{item.box?.type === "photo" ? (
+								<Image
+									src={`/media/${item.box?.file}`}
+									width={200}
+									height={200}
+									// className="w-40 h-40 object-contain"
+									alt={item.retailer}
+								/>
+							) : item.box?.type === "video" ? (
+								<video src={`/media/${item.box?.file}`} controls />
+							) : null}
+						</td>
+						<td>{item.payment.options.join(", ")}</td>
+						<td>{item.payment.terms}</td>
+						<td>{item.manufacturing_timeframe}</td>
+						<td>{item.delivery_timeframe}</td>
+						<td>
+							<a href={item.link} target="_blank">
+								{item.link}
+							</a>
+						</td>
+						<td>
+							<a
+								href={item.reviews.link}
+								target="_blank"
+							>{`${item.reviews.num_reviews} ${item.reviews.type} reviews: ${item.reviews.rating}`}</a>
+						</td>
+					</tr>
+				))}
+			</tbody>
+		</table>
 	);
 };
 
