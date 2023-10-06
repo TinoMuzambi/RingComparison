8b160962abc4bda82f02e8f68e2f38e7f4d35c81
Tino <tino.muzambi@uct.ac.za>
2023-10-06 11:50:33 +0200

    Adds null filter on homepage.

--- app/page.tsx
+++ app/page.tsx
@@ -1,8 +1,18 @@
 import Items from "@/components/items";
+import { Filter } from "@/interfaces";
 import { getItems } from "@/utils";
 
 export default async function Home() {
-	const data = await getItems();
+	const filter: Filter = {
+		retailer: null,
+		type: null,
+		colour: null,
+		clarity: null,
+		metal: null,
+	};
+
+	const data = await getItems(filter);
+
 	return (
 		<main className="flex min-h-screen flex-col gap-4">
 			<Items items={data} />
