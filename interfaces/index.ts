e0c1983574b375277391cd399c0f54038ab6813d
Tino <tino.muzambi@uct.ac.za>
2023-10-06 11:33:25 +0200

    Adds filter type.

--- interfaces/index.ts
+++ interfaces/index.ts
@@ -3,3 +3,11 @@ import data from "@/data/rings.json";
 export interface ItemsInterface {
 	items: typeof data;
 }
+
+export type Filter = {
+	retailer: string | null;
+	type: string | null;
+	colour: string | null;
+	clarity: string | null;
+	metal: string | null;
+};
